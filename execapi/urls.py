2f735f876a16a7999b37eb776b9685d11c6063b5
Douglas08Santos <>
2022-11-14 10:57:55 -0300

    add swagger doc

--- execapi/urls.py
+++ execapi/urls.py
@@ -2,6 +2,18 @@ from django.urls import path
 
 from execapi.views import *
 
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title='Exec API',
+        default_version='1.0.0',
+        description='API documentation of App'
+    ),
+    public=True
+)
 urlpatterns = [
     path('accountables/', AccountableAPIView.as_view(), name='accountables'),
     path('addresses/', AddressAPIView.as_view(), name='addresses'),
@@ -11,4 +23,7 @@ urlpatterns = [
     path('phones/institutions/', PhoneInstitutionAPIView.as_view(), name='phones/institutions'),
     path('users/', UserAPIView.as_view(), name='users'),
     path('voluntaries/', VoluntaryAPIView.as_view(), name='voluntaries'),
+
+    #Swagger
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger-schema')
 ]
--- execpenal/settings.py
+++ execpenal/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     # Custom config
     'django_filters',
     'rest_framework',
+    'drf_yasg',
     
     'execapi',
 ]
