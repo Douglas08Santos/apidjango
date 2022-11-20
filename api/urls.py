9d1ac60fa8f9809a2ba782d21ab9456626b5c45d
Douglas08Santos <santosdouglas0808@gmail.com>
2022-11-20 01:46:55 -0300

    corrigindo bug swagger erro 500

--- api/urls.py
+++ api/urls.py
@@ -15,14 +15,14 @@ schema_view = get_schema_view(
     public=True
 )
 urlpatterns = [
-    path('accountables/', AccountableAPIView, name='accountables'),
-    path('addresses/', AddressAPIView, name='addresses'),
-    path('attendences/', AttendenceAPIView, name='attendences'),
-    path('institutions/', InstitutionAPIView, name='institutions'),
-    path('phones/users/', PhoneUserAPIView, name='phones/users'),
-    path('phones/institutions/', PhoneInstitutionAPIView, name='phones/institutions'),
-    path('users/', UserAPIView, name='users'),
-    path('voluntaries/', VoluntaryAPIView, name='voluntaries'),
+    path('accountables/', AccountableAPIView.as_view(), name='accountables'),
+    path('addresses/', AddressAPIView.as_view(), name='addresses'),
+    path('attendences/', AttendenceAPIView.as_view(), name='attendences'),
+    path('institutions/', InstitutionAPIView.as_view(), name='institutions'),
+    path('phones/users/', PhoneUserAPIView.as_view(), name='phones/users'),
+    path('phones/institutions/', PhoneInstitutionAPIView.as_view(), name='phones/institutions'),
+    path('users/', UserAPIView.as_view(), name='users'),
+    path('voluntaries/', VoluntaryAPIView.as_view(), name='voluntaries'),
 
     #Swagger
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger-schema')
--- api/views.py
+++ api/views.py
@@ -6,7 +6,7 @@ from api.serializers import *
 
 from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, action
 
 from django.utils.decorators import method_decorator
 
@@ -25,14 +25,10 @@ permission_classes = [permissions.IsAuthenticated, ]
 # Create your views here.
 
 # Accountable
-
-# test_param = openapi.Parameter('test', openapi.IN_QUERY, description="test manual param", type=openapi.TYPE_BOOLEAN)
-#accountable_response = openapi.Response('response description', AccountableSerializer)
-
-
-#@swagger_auto_schema(method='get', manual_parameters=[], responses={200: accountable_response})
-@api_view(['GET', 'PUT', 'POST'])
+accountable_response = openapi.Response('response description', AccountableSerializer)
 class AccountableAPIView(APIView):
+    @swagger_auto_schema(method='get', manual_parameters=[], responses={200: accountable_response})
+    @action(detail=True, methods=['GET', 'POST'])
     def get(self, request):
         accountables = Accountable.objects.all()
         serializer = AccountableSerializer(accountables, many=True)
@@ -40,18 +36,17 @@ class AccountableAPIView(APIView):
 
 
 # Address
-# test_param = openapi.Parameter('test', openapi.IN_QUERY, description="test manual param", type=openapi.TYPE_BOOLEAN)
 address_response = openapi.Response('response description', AddressSerializer)
-
-
-@swagger_auto_schema(method='get', manual_parameters=[], responses={200: address_response})
-@api_view(['GET', 'PUT', 'POST'])
 class AddressAPIView(APIView):
+    @swagger_auto_schema(method='get', manual_parameters=[], responses={200: address_response})
+    @action(detail=True, methods=['GET', 'POST'])
     def get(self, request):
         addresses = Address.objects.all()
         serializer = AddressSerializer(addresses, many=True)
         return Response(serializer.data)
 
+    @swagger_auto_schema(method='post', manual_parameters=[], responses={200: address_response})
+    @action(detail=True, methods=['POST'])
     def post(self, request):
         serializer = AddressSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
@@ -61,13 +56,10 @@ class AddressAPIView(APIView):
 
 
 # Attendence
-# test_param = openapi.Parameter('test', openapi.IN_QUERY, description="test manual param", type=openapi.TYPE_BOOLEAN)
 attendence_response = openapi.Response('response description', AttendenceSerializer)
-
-
-@swagger_auto_schema(method='get', manual_parameters=[], responses={200: attendence_response})
-@api_view(['GET', 'PUT', 'POST'])
 class AttendenceAPIView(APIView):
+    @swagger_auto_schema(method='get', manual_parameters=[], responses={200: attendence_response})
+    @action(detail=True, methods=['GET', 'POST'])
     def get(self, request):
         attendences = Attendence.objects.all()
         serializer = AttendenceSerializer(attendences, many=True)
@@ -75,13 +67,10 @@ class AttendenceAPIView(APIView):
 
 
 # Institution
-# test_param = openapi.Parameter('test', openapi.IN_QUERY, description="test manual param", type=openapi.TYPE_BOOLEAN)
 institution_response = openapi.Response('response description', InstitutionSerializer)
-
-
-@swagger_auto_schema(method='get', manual_parameters=[], responses={200: institution_response})
-@api_view(['GET', 'PUT', 'POST'])
 class InstitutionAPIView(APIView):
+    @swagger_auto_schema(method='get', manual_parameters=[], responses={200: institution_response})
+    @action(detail=True, methods=['GET', 'POST'])
     def get(self, request):
         institutions = Institution.objects.all()
         serializer = InstitutionSerializer(institutions, many=True)
@@ -89,13 +78,10 @@ class InstitutionAPIView(APIView):
 
 
 # PhoneInstitution
-# test_param = openapi.Parameter('test', openapi.IN_QUERY, description="test manual param", type=openapi.TYPE_BOOLEAN)
-phoneInstituion_response = openapi.Response('response description', PhoneInstitutionSerializer)
-
-
-@swagger_auto_schema(method='get', manual_parameters=[], responses={200: phoneInstituion_response})
-@api_view(['GET', 'PUT', 'POST'])
+phoneInstitution_response = openapi.Response('response description', PhoneInstitutionSerializer)
 class PhoneInstitutionAPIView(APIView):
+    @swagger_auto_schema(method='get', manual_parameters=[], responses={200: phoneInstitution_response})
+    @action(detail=True, methods=['GET', 'POST'])
     def get(self, request):
         phoneInstituions = PhoneInstitution.objects.all()
         serializer = PhoneInstitutionSerializer(phoneInstituions, many=True)
@@ -103,13 +89,10 @@ class PhoneInstitutionAPIView(APIView):
 
 
 # PhoneUser
-# test_param = openapi.Parameter('test', openapi.IN_QUERY, description="test manual param", type=openapi.TYPE_BOOLEAN)
 phoneUser_response = openapi.Response('response description', PhoneUserSerializer)
-
-
-@swagger_auto_schema(method='get', manual_parameters=[], responses={200: phoneUser_response})
-@api_view(['GET', 'PUT', 'POST'])
 class PhoneUserAPIView(APIView):
+    @swagger_auto_schema(method='get', manual_parameters=[], responses={200: phoneUser_response})
+    @action(detail=True, methods=['GET', 'POST'])
     def get(self, request):
         phoneUsers = PhoneUser.objects.all()
         serializer = PhoneUserSerializer(phoneUsers, many=True)
@@ -117,13 +100,10 @@ class PhoneUserAPIView(APIView):
 
 
 # User
-# test_param = openapi.Parameter('test', openapi.IN_QUERY, description="test manual param", type=openapi.TYPE_BOOLEAN)
 user_response = openapi.Response('response description', UserSerializer)
-
-
-@swagger_auto_schema(method='get', manual_parameters=[], responses={200: user_response})
-@api_view(['GET', 'PUT', 'POST'])
 class UserAPIView(APIView):
+    @swagger_auto_schema(method='get', manual_parameters=[], responses={200: user_response})
+    @action(detail=True, methods=['GET','POST'])
     def get(self, request):
         users = User.objects.all()
         serializer = UserSerializer(users, many=True)
@@ -131,13 +111,10 @@ class UserAPIView(APIView):
 
 
 # Voluntary
-# test_param = openapi.Parameter('test', openapi.IN_QUERY, description="test manual param", type=openapi.TYPE_BOOLEAN)
 voluntary_response = openapi.Response('response description', VoluntarySerializer)
-
-
-@swagger_auto_schema(method='get', manual_parameters=[], responses={200: voluntary_response})
-@api_view(['GET', 'PUT', 'POST'])
 class VoluntaryAPIView(APIView):
+    @swagger_auto_schema(method='get', manual_parameters=[], responses={200: voluntary_response})
+    @action(detail=True, methods=['GET', 'POST'])
     def get(self, request):
         voluntaries = Voluntary.objects.all()
         serializer = VoluntarySerializer(voluntaries, many=True)
